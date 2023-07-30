181059fbe33071389e8091f16acdeeeab958243b
Merlin Cornehl <mcornehl@mkw.eu>
2023-07-30 12:22:15 +0200

    chore: install & configure svelte + TS

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>{{ config('app.name') }}</title>
+    @vite('resources/ts/app.ts')
+  </head>
+  <body>
+    <noscript>
+      <strong>We're sorry but the app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+    </noscript>
+    <div id="app"></div>
+  </body>
+</html>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/ts/app.ts'],
+            refresh: true,
+        }),
+        svelte()
+    ],
+    resolve: {
+        alias: {
+            '$': '/resources/ts',
+        },
+    },
+});
